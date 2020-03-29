0bb95eac17e175393695ef811c7785c2d4ad4960
Timal Peramune <tperamun@uwo.ca>
2020-03-29 14:32:15 -0400

    saving work

---
+++ flaskblog.py
@@ -0,0 +1,28 @@
+from flask import Flask , render_template, url_for
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Timal Peramune',
+        'title': 'Blog Post 1',
+        'content': 'First post content ',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2020'
+
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def hello():
+    return render_template("home.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title='About')
